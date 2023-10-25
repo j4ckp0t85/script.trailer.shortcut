86d742257214e784509b8656f6186ee17e582669
Michele Granato <michele.granato985@gmail.com>
2023-10-25 23:33:12 +0200

    fix for kodi 20

--- default.py
+++ default.py
@@ -2,41 +2,42 @@
 
 import xbmc
 import xbmcaddon
+import xbmcvfs
 import urllib
 
 __addon__               = xbmcaddon.Addon()
 __addon_id__            = __addon__.getAddonInfo('id')
 __addonname__           = __addon__.getAddonInfo('name')
 __icon__                = __addon__.getAddonInfo('icon')
-__addonpath__           = xbmc.translatePath(__addon__.getAddonInfo('path'))
+__addonpath__           = xbmcvfs.translatePath(__addon__.getAddonInfo('path'))
 __lang__                = __addon__.getLocalizedString
 
 class PlayTrailer:
-    
+
     def __init__(self):
-    
+
         self.getTrailer()
-    
+
     def getTrailer(self):
-    
+
         trailer = xbmc.getInfoLabel('ListItem.Trailer')
         if 'plugin://plugin.video.youtube' not in trailer:
             trailer = urllib.quote(trailer).replace('%3A', ':')
-        
+
         self.debug(trailer)
-        
+
         if len(trailer) > 0:
             xbmc.Player().play(trailer)
         else:
             self.notify(__lang__(32000))
             return
-            
+
     def debug(self, msg):
         if 'true' in __addon__.getSetting('debug'):
             xbmc.log('>>>> ' + __addonname__ + ' <<<< ' + msg)
-        
+
     def notify(self, msg):
         if 'true' in __addon__.getSetting('notify'):
             xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + msg.encode('utf-8') + ', 4000, ' + __icon__ + ')')
-        
+
 PlayTrailer()
